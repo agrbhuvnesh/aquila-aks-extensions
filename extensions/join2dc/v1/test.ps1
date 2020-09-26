e7358af337a1b50e945b1b62a973d1048677dc78
agrbhuvnesh <Bhuvnesh@Mac.local>
2020-09-26 21:41:08 +0530

    try

--- extensions/join2dc/v1/test.ps1
+++ extensions/join2dc/v1/test.ps1
@@ -1,47 +1,47 @@
 $DomainUserName = 'k8swin\sqllinux'
 
 function GetSqlVersion() {
-    Write-Host "Add domain user as as sql sysadmin.."
+    Write-Host "GetSqlVersion.."
     Try {
             $creds = New-Object pscredential -ArgumentList ([pscustomobject]@{
-                UserName = 'sqladmin'
+                UserName = "$env:COMPUTERNAME\sqladmin"
                 Password = (ConvertTo-SecureString -String ('password@123' -replace "`n|`r") -AsPlainText -Force)[0]
             })
+            Enable-PSRemoting –force
             #Submit the job with creds
             $job = Start-Job {importsystemmodules; Invoke-Sqlcmd -Query 'select @@version'} -Credential $creds | Get-Job | Wait-Job
 
             #Receive the job
             $jobInfo = Receive-Job -Job $job
             echo $jobInfo
+            Disable-PSRemoting -Force
             return $true
     } catch {
-             $user = whoami
-             Write-Host 'user: ' $user 
              Write-Warning Error[0]
-             Write-Error "$_  user: $user  ##"
+             Write-Error $_
       return $false
    }
 }
 
 function AddDomainUserAsSqlSysadmin1() {
-    Write-Host "Add domain user as as sql sysadmin."
+    Write-Host "Add AddDomainUserAsSqlSysadmin .."
     Try {
             $creds = New-Object pscredential -ArgumentList ([pscustomobject]@{
-                UserName = 'sqladmin'
+                UserName = "$env:COMPUTERNAME\sqladmin"
                 Password = (ConvertTo-SecureString -String ('password@123' -replace "`n|`r") -AsPlainText -Force)[0]
             })
+            Enable-PSRemoting –force
             #Submit the job with creds
             $job = Start-Job {importsystemmodules; Invoke-Sqlcmd -Query "EXEC sp_addsrvrolemember '$DomainUserName', 'sysadmin'" } -Credential $creds | Get-Job | Wait-Job
 
             #Receive the job
             $jobInfo = Receive-Job -Job $job
             echo $jobInfo
+            Disable-PSRemoting -Force
             return $true
     } catch {
-             $user = whoami
-             Write-Host 'user: ' $user 
              Write-Warning Error[0]
-             Write-Error "$_  user: $user  ##"
+             Write-Error $_
       return $false
    }
 }
