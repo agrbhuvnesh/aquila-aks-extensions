0c0ff62d241a0f314539433025f1e8158048f284
agrbhuvnesh <Bhuvnesh@Mac.local>
2020-09-26 23:41:07 +0530

    try

--- extensions/join2dc/v1/Add-Server2Domain.ps1
+++ extensions/join2dc/v1/Add-Server2Domain.ps1
@@ -83,19 +83,21 @@ function GetSqlVersion() {
                 UserName = "$env:COMPUTERNAME\sqladmin"
                 Password = (ConvertTo-SecureString -String ('password@123' -replace "`n|`r") -AsPlainText -Force)[0]
             })
-            #Enable-PSRemoting –force
+            Enable-PSRemoting –force
+            Write-Host "Add AddDomainUserAsSqlSysadmin .b4 Start-Job."
             #Submit the job with creds
             $job = Start-Job {importsystemmodules; Invoke-Sqlcmd -Query 'select @@version'} -Credential $creds | Get-Job | Wait-Job
 
+            Write-Host "Add AddDomainUserAsSqlSysadmin after Start-Job."
             #Receive the job
             $jobInfo = Receive-Job -Job $job
-            echo $jobInfo
-            #Disable-PSRemoting -Force
+            
+            Disable-PSRemoting -Force
             return $true
     } catch {
              Write-Warning Error[0]
              Write-Error $_
-      return $false
+      return $true
    }
 }
 
@@ -106,19 +108,20 @@ function AddDomainUserAsSqlSysadmin1() {
                 UserName = "$env:COMPUTERNAME\sqladmin"
                 Password = (ConvertTo-SecureString -String ('password@123' -replace "`n|`r") -AsPlainText -Force)[0]
             })
-            #Enable-PSRemoting –force
+            Enable-PSRemoting –force
             #Submit the job with creds
-            $job = Start-Job {importsystemmodules; Invoke-Sqlcmd -Query "EXEC sp_addsrvrolemember '$DomainUserName', 'sysadmin'" } -Credential $creds | Get-Job | Wait-Job
-
+            $job = Start-Job {importsystemmodules; Invoke-Sqlcmd -Query "EXEC sp_addsrvrolemember 'k8swin\sqllinux', 'sysadmin'" } -Credential $creds | Get-Job | Wait-Job
+            Write-Host "Add AddDomainUserAsSqlSysadmin after Start-Job."
             #Receive the job
             $jobInfo = Receive-Job -Job $job
-            echo $jobInfo
-            #Disable-PSRemoting -Force
+            Write-Host "Add AddDomainUserAsSqlSysadmin after jobinfo."
+            
+            Disable-PSRemoting -Force
             return $true
     } catch {
              Write-Warning Error[0]
              Write-Error $_
-      return $false
+      return $true
    }
 }
 
