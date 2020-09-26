c2fe976159ef30b5120fb6b9e8e1ec7cef4d7692
agrbhuvnesh <Bhuvnesh@Mac.local>
2020-09-26 21:56:24 +0530

    try

--- extensions/join2dc/v1/test.ps1
+++ extensions/join2dc/v1/test.ps1
@@ -1,4 +1,41 @@
 $DomainUserName = 'k8swin\sqllinux'
+$DomainPassword = 'password@123'
+$DCIP = '10.240.0.159'
+
+function ChangeDNS() {
+    Write-Host "Changing DNS..."
+    Try {
+        if ($null -eq $DCIP) {
+		    Write-Host "No valid $DCIP"
+            return $false
+        }
+        Write-Host "Changing DNS to $DCIP"
+        $Adapter = Get-NetAdapter | Where-Object {$_.Name -like "Ethernet 2"}
+        Set-DnsClientServerAddress -InterfaceIndex ($Adapter).ifIndex -ServerAddresses $DCIP
+        return $true
+    } catch {
+        return $false
+    }
+}
+
+function JoinDomain() {
+    Write-Host "Join to domain..."
+    $joinCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
+        UserName = $DomainUserName
+        Password = (ConvertTo-SecureString -String ($DomainPassword -replace "`n|`r") -AsPlainText -Force)[0]
+    })
+    Try {
+        Add-Computer -Domain $DomainName -Credential $joinCred
+		Start-Sleep 10
+        return $true
+    } catch {
+	    Write-Warning Error[0]
+		Write-Error $_
+        Start-Sleep 10
+        return $false
+    }
+
+}
 
 function GetSqlVersion() {
     Write-Host "GetSqlVersion.."
@@ -45,7 +82,8 @@ function AddDomainUserAsSqlSysadmin1() {
       return $false
    }
 }
-
+ChangeDNS
+JoinDomain
 GetSqlVersion
 AddDomainUserAsSqlSysadmin1
 
